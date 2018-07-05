63be25f72b6f72c26def79d2250d227cbea39acd
moedes <jmozes50@gmail.com>
2018-07-05 16:10:23 -0600

    Update example.tf

--- example.tf
+++ example.tf
@@ -1,7 +1,5 @@
 provider "aws" {
-  access_key = "AKIAJOBEIZQH7ZZFXBOA"
-  secret_key = "q7ahydiOGwZmyPpL+CQzsbdMru8xt/IZeyOcYBsg"
-  region     = "us-east-1"
+  
 }
 
 resource "aws_instance" "example" {
